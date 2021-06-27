927a3de10b3fa8fac732882d74d16c2ec9549ecd
Chathurya Wijerathna <8857147+webx-dev@users.noreply.github.com>
2021-06-27 12:21:55 +0530

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,37 @@
+--[[
+    bexolumu-gtav-loadscreen
+    Created by bexolumu
+
+    For discussions, information on future updates, and more, join
+    Discord: https://discord.gg/XdCPXNU4X4
+
+    Copyright (c) 2020-2021 bexolumu
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+]]--
+name "bexolumu-gtav-loadscreen"
+description "bexolumu-gtav-loadscreen"
+author "bexolumu"
+version "1.0.0"
+
+files {
+    'index.html',
+    'config.json',
+    'css/bootstrap.min.css',
+    'css/styles.min.css',
+    'img/bg-1.jpg',
+    'img/bg-2.jpg',
+    'img/loader.gif',
+    'img/logo.png',
+    'js/bootstrap.min.js',
+    'js/jquery-3.6.0.min.js',
+    'js/main.min.js',
+}
+
+loadscreen 'index.html'
